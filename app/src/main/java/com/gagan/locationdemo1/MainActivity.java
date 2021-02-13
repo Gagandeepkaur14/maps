105174d2ecab82cec6f05b66ffb73f10a0d34908
Gagandeepkaur14 <gagankamboj14@gamil.com>
2021-02-13 01:56:01 -0500

    all

---
+++ app/src/main/java/com/gagan/locationdemo1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.gagan.locationdemo1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 2nd step is declaring location manager and location listener
+    LocationManager locationManager;
+    LocationListener locationListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 3rd step instantiate the location manager and location listener
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.i
+            }
+
+
+        };
+
+        // 3rd step request for the permission
+        ActivityCompat.requestPermissions();
+
+
+
+    }
+}
